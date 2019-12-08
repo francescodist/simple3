bee992db87a0092dbecbea23fe2365908f7bc8fc
francescodist <francesco.dstf@gmail.com>
2019-12-08 17:35:37 +0100

    Fix simple params

--- src/app/services/simple.service.ts
+++ src/app/services/simple.service.ts
@@ -10,14 +10,14 @@ export class SimpleService {
   constructor(private http: HttpClient) {}
 
   public getSimplifiedText(text: string): Observable<SimplifiedResult> {
-    const params = new HttpParams();
-    params.append("textarea", text);
-    params.append("type", "json");
+    const params = new HttpParams()
+      .set("textarea", text)
+      .set("type", "json");
     const headers = new HttpHeaders();
     headers.append("Access-Control-Allow-Origin", "*");
     return this.http.get<SimplifiedResult>(
       "http://www.math.unipa.it/simplehealth/simple/service/ita",
-      { headers }
+      { headers, params }
     );
   }
 }
