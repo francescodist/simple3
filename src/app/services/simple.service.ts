fbe3ef392dc0b2ab900b7b02dd4aabfca67d4240
cicciodist <fstefano@zinio.com>
2019-12-08 16:40:09 +0100

    Add service for simple

--- src/app/services/simple.service.ts
+++ src/app/services/simple.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { SimplifiedResult } from "../models/simplified-term";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class SimpleService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  public getSimplifiedText(text: string): Observable<SimplifiedResult> {
+    const params = new HttpParams();
+    params.append("textarea", text);
+    params.append("type", "json");
+    return this.http.get<SimplifiedResult>(
+      "http://www.math.unipa.it/simplehealth/simple/service/eng"
+    );
+  }
 }
