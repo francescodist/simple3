8c2c357b5e57c47dab9926f47bd880a3f33bc128
francescodist <francesco.dstf@gmail.com>
2019-12-09 09:09:45 +0100

    Add result to Simple service

--- src/app/services/simple.service.ts
+++ src/app/services/simple.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpParams, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs";
-import { SimplifiedResult } from "../models/simplified-term";
+import { SimplifiedResult, SimplifiedTerm } from "../models/simplified-term";
 
 @Injectable({
   providedIn: "root"
 })
 export class SimpleService {
+
+  private result: SimplifiedResult;
+
   constructor(private http: HttpClient) {}
 
   public getSimplifiedText(text: string): Observable<SimplifiedResult> {
@@ -19,5 +22,13 @@ export class SimpleService {
       "http://www.math.unipa.it/simplehealth/simple/service/ita",
       { headers, params }
     );
+  }
+
+  public setResult(result: SimplifiedResult) {
+    this.result = result;
+  }
+
+  public getResult(): SimplifiedTerm[] {
+    return this.result[0].contenuto;
   }
 }
