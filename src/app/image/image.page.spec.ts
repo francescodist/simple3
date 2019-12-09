61f5801f9e416e79bb475bb7c1e8955896174fea
francescodist <francesco.dstf@gmail.com>
2019-12-09 09:29:46 +0100

    Add tab for images

--- src/app/image/image.page.spec.ts
+++ src/app/image/image.page.spec.ts
@@ -1,19 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { Tab2Page } from './tab2.page';
+import { ImagePage } from './image.page';
 
-describe('Tab2Page', () => {
-  let component: Tab2Page;
-  let fixture: ComponentFixture<Tab2Page>;
+describe('ImagePage', () => {
+  let component: ImagePage;
+  let fixture: ComponentFixture<ImagePage>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [Tab2Page],
+      declarations: [ImagePage],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(Tab2Page);
+    fixture = TestBed.createComponent(ImagePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
---
+++ src/app/image/image.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image',
+  templateUrl: 'image.page.html',
+  styleUrls: ['image.page.scss']
+})
+export class ImagePage {
+
+  constructor() {}
+
+}
