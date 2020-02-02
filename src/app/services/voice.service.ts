8eba6de7e6898e5142d0db7f5ea9b6501b341684
francescodist <francesco.dstf@gmail.com>
2020-02-02 19:01:23 +0000

    Add voice service features

--- src/app/services/voice.service.ts
+++ src/app/services/voice.service.ts
@@ -2,22 +2,26 @@ import { Injectable } from "@angular/core";
 import { Camera, CameraOptions } from "@ionic-native/camera/ngx";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { SpeechRecognition } from '@ionic-native/speech-recognition/ngx';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: "root"
 })
 export class VoiceService {
-    constructor(private speechRecognition: SpeechRecognition) {}
+    constructor(private speechRecognition: SpeechRecognition) {
+        this.speechRecognition.hasPermission()
+            .then((hasPermission: boolean) => {
+                if (!hasPermission) {
+                    this.speechRecognition.requestPermission();
+                }
+            });
+    }
 
-    public startRecording() {
-        this.speechRecognition.startListening()
-            .subscribe(
-                (matches: string[]) => alert(matches),
-                (onerror) => console.log('error:', onerror)
-            )
+    public startRecording(): Observable<string[]> {
+        return this.speechRecognition.startListening();
     }
 
     public stopRecording() {
-
+        this.speechRecognition.stopListening();
     }
 }
--- src/app/voice/voice.page.ts
+++ src/app/voice/voice.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { VoiceService } from '../services/voice.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-voice',
@@ -8,15 +9,21 @@ import { VoiceService } from '../services/voice.service';
 })
 export class VoicePage {
   isRecording = false;
-  constructor(private voiceService: VoiceService) {}
+  constructor(private voiceService: VoiceService, private route: Router) {}
 
   public startRecording() {
     this.isRecording = true;
-    this.voiceService.startRecording()
+    this.voiceService.startRecording().subscribe(matches => {
+      const text = matches[0];
+      if(matches && matches[0]) {
+        this.isRecording = false;
+        this.route.navigate(['tabs','text'], {queryParams: {text}});
+      }
+    }).add(() => this.isRecording = false)
   }
 
   public stopRecording() {
-    this.isRecording = false;
+    this.voiceService.stopRecording();
   }
 
   public getRecordText(): string {
