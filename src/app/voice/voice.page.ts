ac07ab85cc6c93cc14c38610f3906ecf82b563fa
francescodist <francesco.dstf@gmail.com>
2020-02-02 17:35:22 +0100

    Add layout for voice tab

--- src/app/voice/voice.page.ts
+++ src/app/voice/voice.page.ts
@@ -6,7 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['voice.page.scss']
 })
 export class VoicePage {
-
+  isRecording = false;
   constructor() {}
 
+  public startRecording() {
+    this.isRecording = true;
+  }
+
+  public stopRecording() {
+    this.isRecording = false;
+  }
+
+  public getRecordText(): string {
+    return this.isRecording ? 'Interrompi Registrazione' : 'Avvia Registrazione';
+  }
 }
